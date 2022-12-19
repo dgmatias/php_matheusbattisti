ed548f77fbe27e20acc78ecaffefc6702c29c56e
Diego Matias <dg.matias021@gmail.com>
2022-12-18 23:15:24 -0300

    adicionando os exercicios 1 e 2

---
+++ ex1/index.php
@@ -0,0 +1,14 @@
+<!-- 
+
+Crie um arquivo PHP
+Este arquivo deverá ter uma instrução que imprime seu nome
+Execute o arquivo
+
+ -->
+
+<?php
+
+    echo "Hello Word! Meu nome é Diego";
+
+
+?>
---
+++ ex2/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = "Diego Matias";
+
+$idade = 17;
+
+$objetivo = "ser melhor que ontem";
+
+echo "O meu nome é $nome , tenho $idade anos e meu objetivo é $objetivo";
