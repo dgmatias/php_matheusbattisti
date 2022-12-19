d7ab48ee84dec02bf3286d0ad74638b9b75056a8
Diego Matias <dg.matias021@gmail.com>
2022-12-19 14:56:12 -0300

    adicionando os exercicios da sessão 4

---
+++ ex8/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nome = "Diego";
+    
+    $idade = 17;
+    
+    $metas = [
+        'meta1' => 'Ser melhor que ontem',
+        'meta2' => 'conseguir o primeoro emprego'
+    ];
+
+    $meta1 = $metas['meta1'] ;
+
+    echo "Meu nome é $nome tenho $idade anos e a minha meta é $meta1";
---
+++ ex9/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $n1 = 10;
+
+    $n2 = 5;
+
+    $total = $n1 + $n2; 
+    
+    echo "A soma entre $n1 e $n2 é $total";
